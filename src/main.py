b8c2a7ef566316e3260aa4fec80a2a14bfd87b80
xtsank <etima16122005@gmail.com>
2025-07-18 12:17:44 +0300

    added gitignore

--- src/main.py
+++ src/main.py
@@ -3,10 +3,6 @@ from src.api.router import main_router
 from fastapi import FastAPI
 import uvicorn
 from fastapi.staticfiles import StaticFiles
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent))
 
 
 app = FastAPI()
